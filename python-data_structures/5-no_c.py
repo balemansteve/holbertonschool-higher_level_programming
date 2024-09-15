b1afddd4576a50c1d2216bbe195a2f95bfd4da4c
balemansteve <balemansteve@gmail.com>
2024-09-15 20:10:56 -0300

    Fixed conditional

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -2,8 +2,6 @@
 def no_c(my_string):
     no_c = ""
     for i in my_string:
-        if i == "c" or i == "C":
-            break
-        else:
+        if i != "c" and i != "C":
             no_c += i
     return no_c
