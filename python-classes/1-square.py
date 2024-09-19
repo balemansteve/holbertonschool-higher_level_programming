d5842ad2d64c2296fe11670f42b24cf09b815fb4
balemansteve <balemansteve@gmail.com>
2024-09-18 21:00:25 -0300

    square

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''Class Square that defines a square'''
+
+
+class Square:
+    '''Class with an attribute'''
+    def __init__(self, size):
+        self.__size = None
+        '''Size attribute is defined'''
+    pass
