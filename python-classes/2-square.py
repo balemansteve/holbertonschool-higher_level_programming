de0f88ffa70bedf208379ba8cc6e22ec702b5d55
balemansteve <balemansteve@gmail.com>
2024-09-18 23:35:34 -0300

    square / added conditional

---
+++ python-classes/2-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''Class Square that defines a square'''
+
+
+class Square:
+    '''Class with an attribute'''
+    def __init__(self, size=0):
+        self.__size = size
+        '''Size attribute is defined'''
+        if not isinstance(size, int):
+            raise ValueError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+    pass
