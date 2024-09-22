638b3799b0c7e3ceb538d0c6315649bb001c628f
balemansteve <balemansteve@gmail.com>
2024-09-22 13:48:35 -0300

    square / added area method

---
+++ python-classes/3-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+'''Class Square that defines a square'''
+
+
+class Square:
+    '''Class with an attribute'''
+    def __init__(self, size=0):
+        self.__size = size
+        '''Size attribute is defined'''
+        if not isinstance(size, int):
+            raise ValueError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+    def area(self):
+        '''Area method is defined'''
+        return self.__size * self.__size
+    pass
