542112de635ab7bb6880fa82ae14c72b4de3b16e
balemansteve <balemansteve@gmail.com>
2024-10-06 23:09:22 -0300

    Added comments

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+'''Function that reads file'''
+
+
 def read_file(filename=""):
+    '''Block with that open file'''
     with open(filename, 'r') as f:
         file = f.read()
         print(file)
