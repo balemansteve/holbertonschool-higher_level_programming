de457c3eebb7427ecdfb8b4ed190752a16c0d7ce
balemansteve <balemansteve@gmail.com>
2024-10-16 23:30:50 -0300

    Fixed print, added "Status Code"

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -4,7 +4,7 @@ import csv
 def fetch_and_print_posts():
     url = "https://jsonplaceholder.typicode.com/posts"
     response = requests.get(url)
-    print(response.status_code)
+    print(f"Status Code: {response.status_code}")
 
     if response.status_code == 200:
         posts = response.json()
