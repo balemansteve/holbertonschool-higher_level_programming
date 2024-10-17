f9f92d6b23e0eccaeb557b50652667e409fb3dff
balemansteve <balemansteve@gmail.com>
2024-10-16 23:24:52 -0300

    request

---
+++ restful-api/task_02_requests.py
@@ -0,0 +1,26 @@
+import requests
+import csv
+
+def fetch_and_print_posts():
+    url = "https://jsonplaceholder.typicode.com/posts"
+    response = requests.get(url)
+    print(response.status_code)
+
+    if response.status_code == 200:
+        posts = response.json()
+        for post in posts:
+            print(post['title'])
+
+def fetch_and_save_posts():
+    url = "https://jsonplaceholder.typicode.com/posts"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        posts = response.json()
+        post_list = []
+        for post in posts:
+            post_list.append({
+                'id': post['id'],
+                'title': post['title'],
+                'body': post['body'],
+            })
