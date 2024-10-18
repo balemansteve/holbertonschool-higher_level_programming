bda62db7d9ad511de02d3de0b59fbd81a2d88b33
balemansteve <balemansteve@gmail.com>
2024-10-18 01:55:24 -0300

    Developed a simple API using Python with Flask

---
+++ restful-api/task_04_flask.py
@@ -0,0 +1,45 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+    
+users = {}
+
+@app.route('/')
+def home():
+    return "Welcome to the Flask API!"
+
+@app.route('/data')
+def data():
+    return jsonify(list(users.keys()))
+
+@app.route('/status')
+def status():
+    return "OK"
+
+@app.route('/users/<username>')
+def getuser(username):
+    username = username.lower()
+    user = users.get(username)
+    if user:
+        return jsonify(user)
+    else:
+        return jsonify({"error": "User not found"}), 404
+
+@app.route('/add_user', methods = ['POST'])
+def add_user():
+    data = request.get_json()
+    username = data.get('username')
+    if not username:
+        return jsonify({"error": "Username is required"}), 400
+    if username in users:
+        return jsonify({"error": "User already exists"}), 400
+    users[username] = {
+        "username": username,
+        "name": data.get('name'),
+        "age": data.get('age'),
+        "city": data.get('city')
+    }
+    return jsonify({"message": "User added"}), 201
+
+if __name__ == '__main__':
+    app.run(debug=True)
