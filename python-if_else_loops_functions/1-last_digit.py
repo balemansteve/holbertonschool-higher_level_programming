c8c6f643f2aae543e2ca274fa590503677f0f303
balemansteve <balemansteve@gmail.com>
2025-01-19 21:10:41 -0300

    Decrease the lenght of the line

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -11,4 +11,5 @@ if number >= 0:
     else:
         print("and is less than 6 and not 0")
 else:
-    print(f"Last digit of {number} is {-last_digit} and is less than 6 and not 0")
+    print(f"Last digit of {number} is {-last_digit} "
+          "and is less than 6 and not 0")
