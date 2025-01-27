229ebeaaae4d7adccf53878680437cad3176f27c
balemansteve <balemansteve@gmail.com>
2025-01-27 02:33:24 +0000

    Delete try and finally sentences

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     num = 0
-    try:
-        for i in range(x):
-            if isinstance(my_list[i], int):
-                print("{:d}".format(my_list[i]), end="")
-                num += 1
-    except IndexError:
-        pass
-    finally:
-        print()
-        return num
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end='')
+            num += 1
+        except (ValueError, TypeError):
+            continue
+    print()
+    return num
