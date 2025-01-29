de8a7e389d4f69697de2e03db722485c047bbc5d
balemansteve <balemansteve@gmail.com>
2025-01-29 17:19:10 +0000

    Write a Class with a private attribute

---
+++ python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Class Square
+"""
+
+
+class Square:
+    """
+    Class Square with a private attribute
+    """
+    def __init__(self, size):
+        self.__size = size
