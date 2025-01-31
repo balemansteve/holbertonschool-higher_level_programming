e18cbcc1443a9ebfa283e5f6115fcb267015afb5
balemansteve <balemansteve@gmail.com>
2025-01-31 22:25:09 +0000

    Delete whitespace and comment edit

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -7,7 +7,7 @@ This module defines the Square class
 class Square:
     """
     Represents a Square class witn a private attribute
-    and 
+    and area method implementation
     """
     def __init__(self, size=0):
         self.__size = size
@@ -15,6 +15,6 @@ class Square:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
-    
+
     def area(self):
         return self.__size * self.__size
