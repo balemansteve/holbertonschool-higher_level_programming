0375c351098cd87f529fcdd41ffad1fad54af6c6
balemansteve <balemansteve@gmail.com>
2025-02-09 20:18:37 +0000

    Write a list class and method to print sorted list

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+This module defines a list class 
+"""
+
+
+class MyList(list):
+    """
+    This method prints the sorted list
+    """
+    def print_sorted(self):
+        print(sorted(self))
