00b6d19be0840181557670eb68aedfd6eae1673c
balemansteve <balemansteve@gmail.com>
2025-02-09 21:13:05 +0000

    Fix implementation

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -9,6 +9,5 @@ class MyList(list):
     This method prints the sorted list
     """
     def print_sorted(self):
-        sorted_list = sorted(self)
-        print(sorted_list)
-        return(sorted_list)
+        print(sorted(self))
+        return(sorted(self))
