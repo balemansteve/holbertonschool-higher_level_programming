e521d83f57dc97925e0627688fcc29a132238487
balemansteve <balemansteve@gmail.com>
2025-02-09 16:30:03 +0000

    Add documentation

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python3
+"""
+This module defines a program to print attributes and methods
+"""
+
+
 def lookup(obj):
+    """
+    This method return 'dir' function to list all object attributes
+    and methods
+    """
     return dir(obj)
