84d7fcab1535e6ca4d4bcd9cc18f145531abb243
balemansteve <balemansteve@gmail.com>
2025-02-10 02:28:20 +0000

    Write an empty class with publics instance methods

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+Module for BaseGeometry class
+"""
+
+
+class BaseGeometry:
+    """
+    Empty class
+    """
+
+    def area(self):
+        """
+        Public instance method to calculate the area of the geometry
+        Raises:
+            Exception: If the method has not been implemented.
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        Validates the value
+        Args:
+            name (str): The name
+            value (int): The value
+        Raises:
+            TypeError: If value is not an integer
+            ValueError: If value is less or equal to 0
+        """
+        if type(value) not int:
+            raise ValueError(f'{name} must be an integer')
+        if value <= 0:
+            raise ValueError(f'{name} must be greater than 0')
+        else:
+            return True
