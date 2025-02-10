c91a65c7cc726bbd424a77a275d516445bfac47a
balemansteve <balemansteve@gmail.com>
2025-02-10 02:42:59 +0000

    Write  class to inherit from another class

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+Module for Rectangle
+"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """
+    A class Rectangle that inherits from BaseGeometry
+    Args:
+        BaseGeometry (class): The base class
+    """
+    def __init__(self, width, height):
+        """
+        Initialize a new Rectangle instance
+        Args:
+            width (int): The width of the rectangle
+            height (int): The height of the rectangle
+        """
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """
+        Return the area of the rectangle
+        """
+        return self.width * self.height
+
+    def __str__(self):
+        """
+        Return the string representation of the rectangle
+        """
+        return "[Rectangle] {}/{}".format(self.width, self.height)
