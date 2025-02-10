e817ca0d22bd22204e679c414c8c1175d4e3ff08
balemansteve <balemansteve@gmail.com>
2025-02-10 02:20:20 +0000

    Write empty class with an instance method

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Module for BaseGeometry class
+"""
+
+
+class BaseGeometry:
+    """
+    Empty class
+    """
+    pass
+
+    def area(self):
+        """
+        Public instance method to calculate the area of the geometry
+        Raises:
+            Exception: If the method has not been implemented
+        """
+        raise Exception("area() is not implemented")
