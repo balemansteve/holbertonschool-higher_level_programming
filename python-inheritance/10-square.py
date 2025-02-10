493502672f2a4302cacf862cd58aef8f77c1c3a3
balemansteve <balemansteve@gmail.com>
2025-02-10 02:47:25 +0000

    Write a class to inherit privates attributes

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Module for Square
+"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    A class Square that inherits from Rectangle
+    Args:
+        Rectangle (class): The base class
+    """
+    def __init__(self, size):
+        """
+        Initialize a new Square instance
+        Args:
+            size (int): The size of the square
+        """
+        self.integer_validator('size', size)
+        self.__size = size
+
+    def area(self):
+        """
+        Return the area of the square
+        """
+        return self.__size * 2
