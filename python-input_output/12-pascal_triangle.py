6b0239ebe8384ccbccd0bcb122769b2e9eee7220
balemansteve <balemansteve@gmail.com>
2025-02-16 23:12:00 -0300

    Deleted whitespace

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -7,7 +7,7 @@ Pascal's Triangle
 def pascal_triangle(n):
     """
     Returns a list of lists of integers representing
-    the Pascal’s triangle of n 
+    the Pascal’s triangle of n
     """
     if n <= 0:
         return []
