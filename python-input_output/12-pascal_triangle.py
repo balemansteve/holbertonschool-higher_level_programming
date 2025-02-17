88441c5c6ca268427cf367f79ed2db0bb6b92ce5
balemansteve <balemansteve@gmail.com>
2025-02-16 23:13:10 -0300

    Fixed logic in loop

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -15,8 +15,8 @@ def pascal_triangle(n):
     for i in range(1, n):
         row = [1]
         last_row = triangle[-1]
-        row += [last_row[j] + last_row[j]
-                for j in range(len(last_row))]
+        row += [last_row[j] + last_row[j + 1]
+                for j in range(len(last_row) - 1)]
         row.append(1)
         triangle.append(row)
     return triangle
