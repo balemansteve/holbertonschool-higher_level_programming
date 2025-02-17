26857030d587b532d3795c841bd5db215b273184
balemansteve <balemansteve@gmail.com>
2025-02-16 23:32:30 -0300

    Convert CSV Data to JSON format

---
+++ python-serialization/task_02_csv.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Converting CSV Data to JSON Format
+"""
+import csv
+import json
+
+
+def convert_csv_to_json(csv_filename):
+    """
+    Convert CSV data to JSON and write it to 'data.json'
+    Args:
+        csv_filename (str): The name of the CSV file to be converted
+    Returns:
+        bool: True if the conversion was successful, False otherwise
+    """
+    try:
+        with open(csv_filename, 'r') as csv_file:
+            reader = csv.DictReader(csv_file)
+            data = list(reader)
+
+        with open('data.json', 'w') as json_file:
+            json.dump(data, json_file)
+
+        return True
+    except Exception as e:
+        """
+        Print an error message if conversion fails
+        """
+        print(f"An error occurred: {e}")
