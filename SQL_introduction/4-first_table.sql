57e9d0fc2a0c35fdac4cd7bbfcd2138660cf8017
balemansteve <balemansteve@gmail.com>
2025-03-16 22:04:16 -0300

    Fixed syntax errors

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,3 +1,3 @@
 -- Create a table if it does not exist
 
-CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256);
+CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
--- SQL_introduction/7-insert_value.sql
+++ SQL_introduction/7-insert_value.sql
@@ -1,4 +1,4 @@
 -- Insert a new record into the table
 
 INSERT INTO first_table (id, name)
-VALUES (89, Best School);
+VALUES (89, 'Best School');
