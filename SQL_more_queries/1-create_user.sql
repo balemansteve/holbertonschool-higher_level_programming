2b306cae1daac00b5f6edf130d99f044e6e64b84
balemansteve <balemansteve@gmail.com>
2025-03-16 22:59:42 -0300

    Create user_0d_1 with all privileges and password

---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,5 @@
+-- Create a user with full privileges on all databases and tables
+
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+FLUSH PRIVILEGES;
