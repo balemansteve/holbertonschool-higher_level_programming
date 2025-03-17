6227212497934956560cf806dd2c6c76733a7ec4
balemansteve <balemansteve@gmail.com>
2025-03-16 22:05:54 -0300

    Insert multiple records

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -5,3 +5,13 @@ CREATE TABLE IF NOT EXISTS second_table (
     name VARCHAR(256),
     score INT
 );
+
+
+-- Insert multiple records into the table  
+
+INSERT INTO second_table (id, name, score)
+VALUES
+    (1, 'John', 10),
+    (2, 'Alex', 3),
+    (3, 'Bob', 14),
+    (4, 'George', 8);
