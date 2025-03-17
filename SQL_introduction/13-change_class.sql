8f7da9489535389bde9c6bed4e40dbff2b50baac
balemansteve <balemansteve@gmail.com>
2025-03-16 22:10:04 -0300

    Delete record

--- SQL_introduction/13-change_class.sql
+++ SQL_introduction/13-change_class.sql
@@ -1,5 +1,4 @@
--- Update the score of a specific record
+-- Delete records with a score less than or equal to 5
 
-UPDATE second_table
-SET score = 10
-WHERE name = Bob;
+DELETE FROM second_table
+WHERE score <= 5;
