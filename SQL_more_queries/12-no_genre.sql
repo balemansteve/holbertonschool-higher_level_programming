7686d632af55c1a322d5f17cd421b2b1d8e231ee
balemansteve <balemansteve@gmail.com>
2025-03-16 23:23:26 -0300

    List shows without a genre

---
+++ SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,7 @@
+-- List tv shows that have no associated genre, using LEFT JOIN
+
+SELECT tv_shows.title, IFNULL(tv_show_genres.genre_id, 'NULL') AS genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_show_genres.genre_id IS NULL
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
